25322bb1242d462f12a8f5bd04ea5f9fdb50392e
rruiz <rruiz@techxagon.io>
2026-03-01 13:06:34 -0600

    Add swiftLanguageVersions: [.v5] for Swift 6 compatibility

--- Package.swift
+++ Package.swift
@@ -10,5 +10,6 @@ let package = Package(
     targets: [
         .target(name: "DustCore", path: "Sources/DustCore"),
         .testTarget(name: "DustCoreTests", dependencies: ["DustCore"], path: "Tests/DustCoreTests")
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
